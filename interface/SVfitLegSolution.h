21fd7d6dc611dbb29442057e20ddb9b1906209cb
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-08-28 14:11:13 +0000

    added function to access sum of visible + neutrino momentum in lab frame

--- interface/SVfitLegSolution.h
+++ interface/SVfitLegSolution.h
@@ -15,6 +15,10 @@ class SVfitLegSolution
   /// access to polarization hypothesis used by SVfit in reconstruction of this solution
   polarizationHypothesisType polarizationHypothesis() const { return polarizationHypothesis_; }
 
+  /// access to momentum of visible + invisible decay products
+  /// in laboratory frame
+  reco::Candidate::LorentzVector p4() const { return (p4Vis_ + p4Invis_); }
+
   /// access to momentum of visible decay products,
   /// in laboratory and tau lepton rest-frame
   const reco::Candidate::LorentzVector& p4Vis() const { return p4Vis_; }
@@ -22,7 +26,7 @@ class SVfitLegSolution
 
   /// access to momentum of invisible decay products (neutrinos produced in tau decay);
   /// reconstructed by SVfit algorithm,
-  /// in laboratory and tau lepton rest-frame
+  /// in laboratory and tau lepton rest-frame    
   const reco::Candidate::LorentzVector& p4Invis() const { return p4Invis_; }
   const reco::Candidate::LorentzVector& p4InvisRestFrame() const { return p4InvisRestFrame_; }
 
