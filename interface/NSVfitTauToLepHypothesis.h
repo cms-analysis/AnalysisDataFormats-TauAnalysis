48331ce3bf02899e357bcc72636249019011738a
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-02-27 16:42:10 +0000

    o added tau --> had, tau --> e/mu hypotheses o added dictionaries for nSVfit data-formats

---
+++ interface/NSVfitTauToLepHypothesis.h
@@ -0,0 +1,38 @@
+#ifndef AnalysisDataFormats_TauAnalysis_NSVfitTauToLepHypothesis_h
+#define AnalysisDataFormats_TauAnalysis_NSVfitTauToLepHypothesis_h
+
+#include "AnalysisDataFormats/TauAnalysis/interface/NSVfitSingleParticleHypothesisBase.h"
+
+template <typename T>
+class NSVfitTauToLepHypothesis : public NSVfitSingleParticleHypothesisBase
+{
+ public:
+
+  /// decay angle in tau lepton rest-frame
+  double decay_angle_rf() const { return decay_angle_rf_; }
+
+  /// momenta of visible (electron/muon) and invisible (neutrinos)
+  /// decay products in tau lepton rest-frame
+  const reco::Candidate::LorentzVector& p4vis_rf()   const { return p4vis_rf_;   }
+  const reco::Candidate::LorentzVector& p4invis_rf() const { return p4invis_rf_; }  
+
+  friend class NSVfitTauToLepBuilder;
+
+ private:
+
+  /// decay angle in tau lepton rest-frame
+  double decay_angle_rf_;
+
+  /// momenta of visible (electron/muon) and invisible (neutrinos)
+  /// decay products in tau lepton rest-frame
+  reco::Candidate::LorentzVector p4invis_rf_;
+  reco::Candidate::LorentzVector p4vis_rf_;
+};
+
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+
+typedef NSVfitTauToLepHypothesis<pat::Electron> NSVfitTauToElecHypothesis;
+typedef NSVfitTauToLepHypothesis<pat::Muon>     NSVfitTauToMuHypothesis;
+
+#endif
