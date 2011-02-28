837e67a164bcf79a14b0d501def1f629d575c7d6
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-02-28 16:48:26 +0000

    added visible energy fractions

--- interface/NSVfitTauToLepHypothesis.h
+++ interface/NSVfitTauToLepHypothesis.h
@@ -12,25 +12,39 @@ class NSVfitTauToLepHypothesis : public NSVfitSingleParticleHypothesisBase
     : NSVfitSingleParticleHypothesisBase(particle, name, barcode)
   {}
   ~NSVfitTauToLepHypothesis() {}
-
-  /// decay angle in tau lepton rest-frame
-  double decay_angle_rf() const { return decay_angle_rf_; }
-
+ 
   /// momenta of visible (electron/muon) and invisible (neutrinos)
   /// decay products in tau lepton rest-frame
   const reco::Candidate::LorentzVector& p4vis_rf()   const { return p4vis_rf_;   }
   const reco::Candidate::LorentzVector& p4invis_rf() const { return p4invis_rf_; }  
 
-  friend class NSVfitTauToLepBuilder;
+  /// direction and mass of visible decay products
+  const reco::Candidate::Vector& p3Vis_unit() const { return p3Vis_unit_; }
+  double visMass() const { return visMass_; }
+
+  /// energy ratio of visible decay products/tau lepton energy
+  double visEnFracX() const { return visEnFracX_; }
+
+  /// decay angle in tau lepton rest-frame
+  double decay_angle_rf() const { return decay_angle_rf_; }
+
+  template<typename T_type> friend class NSVfitTauToLepBuilder;
 
  private:
-  /// decay angle in tau lepton rest-frame
-  double decay_angle_rf_;
-
   /// momenta of visible (electron/muon) and invisible (neutrinos)
   /// decay products in tau lepton rest-frame
   reco::Candidate::LorentzVector p4invis_rf_;
   reco::Candidate::LorentzVector p4vis_rf_;
+
+  /// direction and mass of visible decay products
+  reco::Candidate::Vector p3Vis_unit_;
+  double visMass_;
+
+  /// energy ratio of visible decay products/tau lepton energy
+  double visEnFracX_;
+
+  /// decay angle in tau lepton rest-frame
+  double decay_angle_rf_;
 };
 
 #include "DataFormats/PatCandidates/interface/Electron.h"
