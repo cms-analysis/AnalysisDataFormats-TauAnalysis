9361f56d6bdb971fc5455ff6853abdcc52032c3f
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-03-23 17:45:18 +0000

    - added support for tau decay likelihoods with polarization - switch from std::vector<T*> to edm::OwnVector<T>

--- interface/NSVfitTauToLepHypothesis.h
+++ interface/NSVfitTauToLepHypothesis.h
@@ -11,8 +11,41 @@ class NSVfitTauToLepHypothesis : public NSVfitSingleParticleHypothesisBase
   NSVfitTauToLepHypothesis(const edm::Ptr<reco::Candidate>& particle, const std::string& name, int barcode) 
     : NSVfitSingleParticleHypothesisBase(particle, name, barcode)
   {}
-  ~NSVfitTauToLepHypothesis() {}
+  NSVfitTauToLepHypothesis(const NSVfitTauToLepHypothesis& bluePrint)
+    : NSVfitSingleParticleHypothesisBase(bluePrint),
+      p4invis_rf_(bluePrint. p4invis_rf_),
+      p4vis_rf_(bluePrint.p4vis_rf_),
+      p3Vis_unit_(bluePrint.p3Vis_unit_),
+      visMass_(bluePrint.visMass_),
+      visEnFracX_(bluePrint.visEnFracX_),
+      decay_angle_rf_(bluePrint.decay_angle_rf_),
+      decayVertexPos_(bluePrint.decayVertexPos_),
+      flightPath_(bluePrint.flightPath_),
+      decayDistance_(bluePrint.decayDistance_),
+      decayMode_(bluePrint.decayMode_),
+      polarization_(bluePrint.polarization_)
+  {}
+  ~NSVfitTauToLepHypothesis() {}  
+
+  NSVfitSingleParticleHypothesisBase* clone() const { return new NSVfitTauToLepHypothesis(*this); }
  
+  virtual NSVfitSingleParticleHypothesisBase& operator=(const NSVfitTauToLepHypothesis& bluePrint)
+  {
+    NSVfitSingleParticleHypothesisBase::operator =(bluePrint);
+    p4invis_rf_ = bluePrint. p4invis_rf_;
+    p4vis_rf_ = bluePrint.p4vis_rf_;
+    p3Vis_unit_ = bluePrint.p3Vis_unit_;
+    visMass_ = bluePrint.visMass_;
+    visEnFracX_ = bluePrint.visEnFracX_;
+    decay_angle_rf_ = bluePrint.decay_angle_rf_;
+    decayVertexPos_ = bluePrint.decayVertexPos_;
+    flightPath_ = bluePrint.flightPath_;
+    decayDistance_ = bluePrint.decayDistance_;
+    decayMode_ = bluePrint.decayMode_;
+    polarization_ = bluePrint.polarization_;
+    return (*this);
+  }
+
   /// momenta of visible (electron/muon) and invisible (neutrinos)
   /// decay products in tau lepton rest-frame
   const reco::Candidate::LorentzVector& p4vis_rf()   const { return p4vis_rf_;   }
@@ -27,6 +60,19 @@ class NSVfitTauToLepHypothesis : public NSVfitSingleParticleHypothesisBase
 
   /// decay angle in tau lepton rest-frame
   double decay_angle_rf() const { return decay_angle_rf_; }
+
+  /// access to position of secondary vertex (tau lepton decay vertex)
+  bool hasDecayVertex() const { return true; }
+  const AlgebraicVector3& decayVertexPos() const { return decayVertexPos_; }
+  const reco::Candidate::Vector& flightPath() const { return flightPath_; }
+  double decayDistance() const { return decayDistance_; }
+
+  /// tau lepton hadronic decay mode
+  /// (as defined in DataFormats/TauReco/interface/PFTauDecayMode.h)
+  int decayMode() const { return decayMode_; }
+
+  /// tau lepton polarization
+  double polarization() const { return polarization_; }
 
   template<typename T_type> friend class NSVfitTauToLepBuilder;
 
@@ -45,6 +91,17 @@ class NSVfitTauToLepHypothesis : public NSVfitSingleParticleHypothesisBase
 
   /// decay angle in tau lepton rest-frame
   double decay_angle_rf_;
+
+  /// position of reconstructed tau lepton decay vertex
+  AlgebraicVector3 decayVertexPos_;
+  reco::Candidate::Vector flightPath_;
+  double decayDistance_;
+
+  /// tau lepton hadronic decay mode
+  int decayMode_;
+
+  /// tau lepton polarization
+  double polarization_;
 };
 
 #include "DataFormats/PatCandidates/interface/Electron.h"
