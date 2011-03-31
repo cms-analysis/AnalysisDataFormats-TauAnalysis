d012815885001cd63cd3e0fc2fd2a7a6eb93d0a8
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2011-03-31 01:17:07 +0000

    Add missing members to copy constructors

--- src/NSVfitTauDecayHypothesis.cc
+++ src/NSVfitTauDecayHypothesis.cc
@@ -2,7 +2,10 @@
 
 NSVfitTauDecayHypothesis::NSVfitTauDecayHypothesis(const NSVfitTauDecayHypothesis& bluePrint)
   : NSVfitSingleParticleHypothesisBase(bluePrint),
+    p4invis_rf_(bluePrint.p4invis_rf_),
+    p4vis_rf_(bluePrint.p4vis_rf_),
     p3Vis_unit_(bluePrint.p3Vis_unit_),
+    visMass_(bluePrint.visMass_),
     visEnFracX_(bluePrint.visEnFracX_),
     decay_angle_rf_(bluePrint.decay_angle_rf_),
     decayVertexPos_(bluePrint.decayVertexPos_),
@@ -15,7 +18,10 @@ NSVfitTauDecayHypothesis::NSVfitTauDecayHypothesis(const NSVfitTauDecayHypothesi
 NSVfitSingleParticleHypothesisBase& NSVfitTauDecayHypothesis::operator =(const NSVfitTauDecayHypothesis& bluePrint)
 {
   NSVfitSingleParticleHypothesisBase::operator =(bluePrint);
+  p4invis_rf_ = bluePrint.p4invis_rf_;
+  p4vis_rf_ = bluePrint.p4vis_rf_;
   p3Vis_unit_ = bluePrint.p3Vis_unit_;
+  visMass_ = bluePrint.visMass_;
   visEnFracX_ = bluePrint.visEnFracX_;
   decay_angle_rf_ = bluePrint.decay_angle_rf_;
   decayVertexPos_ = bluePrint.decayVertexPos_;
